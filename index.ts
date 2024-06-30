c92b27d6e163a57db4b37235e3d8103c8ff1e698
hirotoKirimaru <nainaistar@gmail.com>
2024-06-30 23:14:57 +0900

    コピペ

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import { App } from '@slack/bolt';
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+app.message('hello', async ({ message, say }) => {
+  await say(`Hey there <@${message.user}>!`);
+});
+
+(async () => {
+  await app.start(process.env.PORT || 3000);
+  console.log('⚡️ Bolt app is running!');
+})();
